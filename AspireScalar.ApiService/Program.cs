395a425f1b5385d5a65710b53d96bf42d40f017a
Justin Lampe <xC0dex@users.noreply.github.com>
2024-10-29 21:22:25 +0100

    feat: Initial app

--- AspireScalar.ApiService/Program.cs
+++ AspireScalar.ApiService/Program.cs
@@ -3,17 +3,14 @@ using AspireScalar.ApiService.Books;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<BookStore>();
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddOpenApi();
 builder.Services.AddProblemDetails();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 app.UseExceptionHandler();
 
-app.UseSwagger();
-
+app.MapOpenApi();
 app.MapBookEndpoints();
 
 app.Run();
